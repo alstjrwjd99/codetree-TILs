4e3dd7bede538d7ea9d669d8aed0dc7b9b977fd8
codeZzankunst <42230162+alstjrwjd99@users.noreply.github.com>
2024-04-10 13:02:43 +0900

    Title: 총점 비교, Time: 149ms, Memory: 26MB, Status: Passed - CodeTree

---
+++ 240410/총점 비교/compare-total-points.py
@@ -0,0 +1,8 @@
+from functools import cmp_to_key
+n = int(input())
+person = []
+for _ in range (n):
+    person.append(tuple(input().split()))
+person.sort(key = lambda x : (int(x[1]) + int(x[2]) + int(x[3])))
+for a in person:
+    print(*a)
