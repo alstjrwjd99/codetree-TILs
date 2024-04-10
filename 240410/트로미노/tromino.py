f9d213b09d707589589b8bde39258ad4ff2a794d
codeZzankunst <42230162+alstjrwjd99@users.noreply.github.com>
2024-04-10 15:28:51 +0900

    Title: 트로미노, Time: 425ms, Memory: 29MB, Status: Passed - CodeTree

--- 240410/트로미노/tromino.py
+++ 240410/트로미노/tromino.py
@@ -10,9 +10,12 @@ def check_nieun(x,y):
     return 0
 
 def check_long(x,y):
-    if 0<=x and x+2 < n and 0<=y and y+2 < m:
-        return max(maps[x][y]+maps[x][y+1]+maps[x][y+2],maps[x][y]+maps[x+1][y]+maps[x+2][y]) 
-    return 0
+    length, width = 0,0
+    if 0<=x and x+2 < n :
+        width = maps[x][y]+maps[x+1][y]+maps[x+2][y]
+    if 0<=y and y+2 < m :
+        length = maps[x][y]+maps[x][y+1]+maps[x][y+2]
+    return max(width,length)
 
 answer = 0
 for i in range (n):
