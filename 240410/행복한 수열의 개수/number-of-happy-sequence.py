df15684866528e329f96c11643d4a84c704772d9
codeZzankunst <42230162+alstjrwjd99@users.noreply.github.com>
2024-04-10 15:06:07 +0900

    Title: 행복한 수열의 개수, Time: 149ms, Memory: 25MB, Status: Passed - CodeTree

--- 240410/행복한 수열의 개수/number-of-happy-sequence.py
+++ 240410/행복한 수열의 개수/number-of-happy-sequence.py
@@ -5,20 +5,19 @@ for _ in range (n):
 
 def check_line(line):
     cont = 1
+    maxcnt = 1
     for i in range (1,n):
         if line[i] == line[i-1]:
             cont += 1
         else :
             cont = 1
-        if cont == m:
-            return True
-    return False
+        maxcnt = max(maxcnt,cont)
+    return maxcnt >= m
 
 answer = 0
 for i in range (n):
     if check_line(grid[i]):
         answer +=1 
-for i in range (n):
     if check_line([grid[j][i] for j in range (n)]):
         answer +=1 
 print(answer)
