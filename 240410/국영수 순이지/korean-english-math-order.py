d4b5fdb54d63a66ad7f70cb400adf19984dae5d7
codeZzankunst <42230162+alstjrwjd99@users.noreply.github.com>
2024-04-10 12:59:09 +0900

    Title: 국영수 순이지, Time: 151ms, Memory: 24MB, Status: Passed - CodeTree

--- 240410/국영수 순이지/korean-english-math-order.py
+++ 240410/국영수 순이지/korean-english-math-order.py
@@ -2,6 +2,6 @@ n = int(input())
 person = []
 for _ in range (n):
     person.append(tuple(input().split()))
-person.sort(key = lambda x : (x[1],x[2],x[3]),reverse = True)
+person.sort(key = lambda x : (-int(x[1]),-int(x[2]),-int(x[3])))
 for a in person:
     print(*a)
