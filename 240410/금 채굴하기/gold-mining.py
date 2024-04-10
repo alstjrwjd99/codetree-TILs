8338f387ec8f02ff4ad036ce3651239fe01cd78e
codeZzankunst <42230162+alstjrwjd99@users.noreply.github.com>
2024-04-10 16:50:30 +0900

    Title: 금 채굴하기, Time: 108ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240410/금 채굴하기/gold-mining.py
@@ -0,0 +1,44 @@
+n,m = map(int,input().split())
+maps = []
+for _ in range (n):
+    maps.append(list(map(int,input().split())))
+
+def in_range(x,y):
+    return 0<=x<n and 0<=y<n
+
+# 마름모 모양 만들기
+def rhombus(x,y,k):
+    k += 1
+    visited = set()
+    for i in range (k):
+        for j in range (i):
+            if in_range(x-k+i,y-j):
+                visited.add((x-k+i,y-j))
+            if in_range(x-k+i,y+j):
+                visited.add((x-k+i,y+j))
+            if in_range(x+k-i,y-j):
+                visited.add((x+k-i,y-j))
+            if in_range(x+k-i,y+j):
+                visited.add((x+k-i,y+j))
+        if in_range(x,y-i):
+            visited.add((x,y-i))
+        if in_range(x,y+i):
+            visited.add((x,y+i))
+    return list(visited)
+
+# 범위내 골드 값 구하기
+# 골드 값 - 마름모 수고비 > 0 인 부분
+def total(x,y,k):
+    earn = 0
+    for a in (rhombus(x,y,k)):
+        if maps[a[0]][a[1]] == 1:
+            earn += 1
+    if (earn*m - (k**2+(k+1)**2)) >= 0:
+        return earn
+    return 0
+
+answer = 0
+for i in range (n):
+    for j in range (n):
+        answer = max(answer,total(i,j,2))
+print(answer)
