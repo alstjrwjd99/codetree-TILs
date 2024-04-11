78988da8b2187c190760d24d15f8a3ca19652e0d
codeZzankunst <42230162+alstjrwjd99@users.noreply.github.com>
2024-04-11 20:14:37 +0900

    Title: 최단 Run Length 인코딩, Time: 109ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240411/최단 Run Length 인코딩/shortest-run-length-encoding.py
@@ -0,0 +1,16 @@
+A = input()
+def encolen(A):
+    en = A[0]
+    cnt = 0
+    for a in A:
+        if en[-1] != a:
+            en += str(cnt) + a
+            cnt = 1
+        else :
+            cnt += 1
+    return en + str(cnt)
+answer = 0
+for i in range (len(A)):
+    b = A[i:] + A[:i]
+    answer = min(len(encolen(b)),answer)
+print(answer)
