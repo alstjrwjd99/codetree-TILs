4ab546160456ac0d1afb2b452cde72ef363d697b
codeZzankunst <42230162+alstjrwjd99@users.noreply.github.com>
2024-04-12 20:58:55 +0900

    Title: 숫자가 가장 큰 인접한 곳으로 동시에 이동, Time: 138ms, Memory: 25MB, Status: Wrong Answer - CodeTree

---
+++ 240412/숫자가 가장 큰 인접한 곳으로 동시에 이동/move-to-max-adjacent-cell-simultaneously.py
@@ -0,0 +1,41 @@
+n, m, t = tuple(map(int, input().split()))
+grid = [
+    list(map(int, input().split()))
+    for _ in range(n)
+]
+biz = []
+for _ in range(m):
+    r,c = list(map(int, input().split()))
+    biz.append((r-1,c-1))   
+
+dx = [-1,1,0,0]
+dy = [0,0,-1,1]
+
+def in_range(x,y):
+    return 0<=x<n and 0<=y<n
+
+def move():
+    for idx,b in enumerate(biz):
+        max_value = 0
+        for i in range (4):
+            nx = b[0] + dx[i]
+            ny = b[1] + dy[i]
+            if in_range(nx,ny):
+                if grid[nx][ny] > max_value:
+                    max_value = grid[nx][ny]
+                    biz[idx] = (nx,ny)
+    removeDup()
+
+def removeDup():
+    dup = [[0]*n for _ in range (n)]
+    for idx,b in enumerate(biz):
+        if dup[b[0]][b[1]] == 0:
+            dup[b[0]][b[1]] = idx+1
+        else :
+            biz.remove(biz[idx])
+            biz.remove(biz[dup[b[0]][b[1]]])
+
+                
+for _ in range (t):
+    move()
+print(len(biz))
