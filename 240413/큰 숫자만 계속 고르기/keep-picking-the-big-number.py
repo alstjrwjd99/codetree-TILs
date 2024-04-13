234fd97ed8d7ec8c168c149539cc1a9ebd144182
codeZzankunst <42230162+alstjrwjd99@users.noreply.github.com>
2024-04-13 11:47:38 +0900

    Title: 큰 숫자만 계속 고르기, Time: 231ms, Memory: 34MB, Status: Passed - CodeTree

--- 240413/큰 숫자만 계속 고르기/keep-picking-the-big-number.py
+++ 240413/큰 숫자만 계속 고르기/keep-picking-the-big-number.py
@@ -1,13 +1,24 @@
 import heapq
 
-n,m = map(int,input().split())
-s = list(map(int,input().split()))
+# 변수 선언 및 입력:
+n, m = tuple(map(int, input().split()))
+arr = list(map(int, input().split()))
+pq = []
 
-hq  =[]
-for i in s: 
-    heapq.heappush(hq,-i)
+# priority queue에
+# 숫자들을 넣어줍니다.
+# 최댓값을 구해야 하므로
+# -를 붙여서 넣어줍니다.
+for elem in arr:
+    heapq.heappush(pq, -elem)
 
-for _ in range (m):
-    hq[0] += 1
-    heapq.heapify(hq)
-print(hq[0]*(-1))
+# m번에 걸쳐서 
+# 최댓값을 찾아 1씩 빼주는 것을 반복합니다.
+for _ in range(m):
+    # 최댓값을 찾고 제거합니다.
+    max_val = -heapq.heappop(pq)
+    # 1 뺀 값을 다시 넣어줍니다.
+    # -를 붙여서 넣어줘야 함에 유의합니다.
+    heapq.heappush(pq, -(max_val - 1))
+
+print(-heapq.heappop(pq))
