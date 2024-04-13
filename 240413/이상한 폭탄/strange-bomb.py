6e568469778e1a926cd55eacb9de3d4fb52115b4
codeZzankunst <42230162+alstjrwjd99@users.noreply.github.com>
2024-04-13 15:11:46 +0900

    Title: 이상한 폭탄, Time: 5102ms, Memory: 37MB, Status: Time Limit Exceed - CodeTree

---
+++ 240413/이상한 폭탄/strange-bomb.py
@@ -0,0 +1,22 @@
+n,m = map(int,input().split())
+bomb  = []
+for _ in range (n):
+    bomb.append(int(input()))
+
+explosion = [False] * n
+for i in range (n):
+    start = 0
+    if i - m >= 0 :
+        start = i-m
+    if bomb[i] in bomb[start:i]:
+        explosion[i] = True
+    end = n
+    if i+m < n:
+        end = i+m+1
+    if bomb[i] in bomb[i+1:end]:
+        explosion[i] = True
+answer = 0
+for idx,a in enumerate(explosion):
+    if a :
+        answer = max(answer,bomb[idx])
+print(answer) if answer !=0 else print(-1)
