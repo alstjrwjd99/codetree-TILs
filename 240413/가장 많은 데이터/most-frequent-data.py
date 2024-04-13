ac2b3f856dd39b6398a5e4b04ec1bc7679b88d28
codeZzankunst <42230162+alstjrwjd99@users.noreply.github.com>
2024-04-13 11:32:36 +0900

    Title: 가장 많은 데이터, Time: 363ms, Memory: 37MB, Status: Passed - CodeTree

--- 240413/가장 많은 데이터/most-frequent-data.py
+++ 240413/가장 많은 데이터/most-frequent-data.py
@@ -1,9 +1,22 @@
-from collections import Counter
-
+# 변수 선언 및 입력:
 n = int(input())
-color = []
-for _ in range (n):
-    color.append(input())
+words = [input() for _ in range(n)]
 
-c = Counter(color)
-print(c[max(c)])
+freq = dict()
+ans = 0
+
+# 각 문자열이 몇 번씩 나왔는지를
+# hashmap에 기록해줍니다.
+for word in words:
+    # 처음 나온 문자열이라면 1을 직접 적어줘야 합니다.
+    if word not in freq:
+        freq[word] = 1
+    # 이미 나와있던 문자열이라면 1을 더해줍니다.
+    else:
+        freq[word] += 1
+
+    # 가장 많이 나온 횟수를
+    # 갱신해줍니다.
+    ans = max(ans, freq[word])
+
+print(ans)
