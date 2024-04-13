f31de07760327abe2b7931d619947769fa313ed2
codeZzankunst <42230162+alstjrwjd99@users.noreply.github.com>
2024-04-13 11:33:17 +0900

    Title: hashset 기본, Time: 432ms, Memory: 28MB, Status: Passed - CodeTree

---
+++ 240413/hashset 기본/hashset-basic.py
@@ -0,0 +1,17 @@
+# 변수 선언 및 입력:
+n = int(input())
+s = set()
+
+for _ in range(n):
+    command, x = tuple(input().split())
+    x = int(x)
+
+    if command == "add":
+        s.add(x)
+    elif command == "remove":
+        s.remove(x)
+    else:
+        if x in s:
+            print("true")
+        else:
+            print("false")
