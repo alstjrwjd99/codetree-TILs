9c7ae4f1c452ba230546dd4661b0d04606d172bd
codeZzankunst <42230162+alstjrwjd99@users.noreply.github.com>
2024-04-15 23:41:50 +0900

    Title: 마을 구분하기, Time: 139ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240415/마을 구분하기/seperate-village.py
@@ -0,0 +1,38 @@
+n = int(input())
+grid = [
+    list(map(int,input().split()))
+    for _ in range(n)
+]
+
+dxs = [0,1,-1,0]
+dys = [1,0,0,-1]
+
+visited = [[0]*n for _ in range (n)]
+global answer
+
+def inrange(x,y):
+    return 0<=x<n and 0<=y<n
+
+def dfs(x,y):
+    global answer
+
+    for i,j in zip(dxs,dys):
+        nx = x + i
+        ny = y + j
+        if inrange(nx,ny) and grid[nx][ny] == 1 and visited[nx][ny] == 0:
+            visited[nx][ny] = 1
+            answer += 1
+            dfs(nx,ny)
+
+tmp = []
+for i in range (n):
+    for j in range (n):
+        answer = 0
+        if grid[i][j] == 1:
+            dfs(i,j)
+            if answer != 0:
+                tmp.append(answer)
+print(len(tmp))
+tmp.sort()
+for a in tmp:
+    print(a)
